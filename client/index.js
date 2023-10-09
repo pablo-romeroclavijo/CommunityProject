642d24c1665a1e5612c4d036ca3a1a6d23f33f6c
DavidEvans <david@evanstmd.plus.com>
2023-10-09 14:30:42 +0100

    Some stuff on the login page

---
+++ client/index.js
@@ -0,0 +1,10 @@
+function revealPassword(){
+    const x = document.getElementById("password")
+    console.log(x)
+    if (x.type === "password"){
+        x.type = "text";
+    }
+    else{
+        x.type = "password"
+    }
+}
