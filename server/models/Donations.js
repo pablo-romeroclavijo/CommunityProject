36b80f4d77be09185a4a8212df82dd60bc37ba4d
JustaGlitch <justingraham1108@gmail.com>
2023-10-10 16:21:23 +0100

    fixed all conflicts

--- server/models/Donations.js
+++ server/models/Donations.js
@@ -20,37 +20,34 @@ class Donation {
 		return new Donation(response.rows[0]);
 	}
 
-    static async create(data){
-        const {user_id, items, slot_time, slot_date, type} = data 
-        const event = await Events.create(type, user_id, slot_time, slot_date)
-        const date = new Date()
+	static async create(data) {
+		const { user_id, items, slot_time, slot_date, type } = data;
+		const event = await Events.create(type, user_id, slot_time, slot_date);
+		const date = new Date();
 
-        const currentDate = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(); 
+		const currentDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 
+		const response = await db.query("INSERT INTO donations (user_id, donation_date, drop_id) VALUES ($1, $2, $3) RETURNING donation_id", [Number(user_id), currentDate, event.id]);
+		const donation = await Donation.getOneById(response.rows[0].donation_id);
+		const query = donation.donation.queryBuilder(items);
 
-        const response = await db.query("INSERT INTO donations (user_id, donation_date, drop_id) VALUES ($1, $2, $3) RETURNING donation_id", [Number(user_id), currentDate, event.id ])
-        const donation = await Donation.getOneById(response.rows[0].donation_id)
-        const query = donation.donation.queryBuilder(items)
+		const responseCreateItems = await db.query(query);
 
+		const itemList = responseCreateItems.rows.map((item) => new ItemDonated(item));
 
-        const responseCreateItems = await db.query(query)
+		return { event, donation, itemList };
+	}
 
-        const itemList = responseCreateItems.rows.map(item => new ItemDonated(item) )
-
-        return {event, donation, itemList} 
-    } 
-
-    queryBuilder(items){
-        let query = 'INSERT INTO items_donated (user_id, donation_id, expiration_date, quantity_donated, quantity_remaining, product_id) VALUES'
-        items.map(item =>{
-            let subquery = "( '" + this.user_id + "', '" + this.id + "', '" + item.expirationDate + "', '" +item.quantityDonated + "', '" + item.quantityDonated + "', '" + item.id + "'),"
-            query = query + subquery
-        })
-        query = query.substring(0, query.length - 1)+ ' RETURNING *;'
-        console.log(query)
-        return query
- 
-    }
+	queryBuilder(items) {
+		let query = "INSERT INTO items_donated (user_id, donation_id, expiration_date, quantity_donated, quantity_remaining, product_id) VALUES";
+		items.map((item) => {
+			let subquery = "( '" + this.user_id + "', '" + this.id + "', '" + item.expirationDate + "', '" + item.quantityDonated + "', '" + item.quantityDonated + "', '" + item.id + "'),";
+			query = query + subquery;
+		});
+		query = query.substring(0, query.length - 1) + " RETURNING *;";
+		console.log(query);
+		return query;
+	}
 
 	static async getAllDonations() {
 		const response = await db.query("SELECT * FROM donations");
@@ -83,15 +80,6 @@ class Donation {
 
 		return { donation, responseItems };
 	}
-        const donation = new Donation(response.rows[0])
-
-        const responseItems = await ItemDonated.getMultipleByDonation(donation.id)
-
-
-        return {donation, responseItems}
-
-    }
 }
-
 
 module.exports = Donation;
